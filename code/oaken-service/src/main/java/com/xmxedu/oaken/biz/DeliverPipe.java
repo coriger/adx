b83a6e876acc5515c40b9715a8caf5e355db2ed7
Maclaon Zen <maclaonzen@gmail.com>
2015-12-09 19:33:30 +0800

    add ad meterial

---
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/biz/DeliverPipe.java
@@ -0,0 +1,11 @@
+package com.xmxedu.oaken.biz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 物料下发通道，对不同的物料下发进行整合，其中类似于spring mvc 的condition一样，提供条件支撑来处理
+ * @version 1.0.0
+ */
+@Service
+public class DeliverPipe {
+}
--- code/oaken-service/src/test/java/com/xmxedu/oaken/mysql/UserInfoJdbcTemplateTest.java
+++ code/oaken-service/src/test/java/com/xmxedu/oaken/mysql/UserInfoJdbcTemplateTest.java
@@ -1,9 +1,7 @@
 package com.xmxedu.oaken.mysql;
 
-import com.xmxedu.oaken.sql.UserInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
