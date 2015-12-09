09c40b3582cfd2f7bc0c4bcedecd6cac7a594861
Maclaon Zen <maclaonzen@gmail.com>
2015-12-09 19:53:14 +0800

    add bidder code of it

---
+++ code/oaken-share/src/main/java/com.xmxedu.oaken/groovy/GroovyEngine.java
@@ -0,0 +1,11 @@
+package com.xmxedu.oaken.groovy;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 用来加载Grooy脚本的引擎，可以自动感知脚本的修改与否的状态
+ * @version 1.0.0
+ */
+@Service
+public class GroovyEngine {
+}
---
+++ code/oaken-share/src/main/java/com/xmxedu/oaken/request/handler/RequestHandler.java
@@ -0,0 +1,11 @@
+package com.xmxedu.oaken.request.handler;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 聚合平台请求方式，POST，GET的方式
+ */
+@Component
+public class RequestHandler {
+    
+}
