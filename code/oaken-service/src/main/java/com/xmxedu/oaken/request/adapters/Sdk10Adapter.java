d2ac7cca5d8d56eab6ce97b2ce0306bd05b9ea33
Maclaon Zen <maclaonzen@gmail.com>
2015-12-16 22:41:08 +0800

    add code  of sdk adapter of it

--- code/oaken-service/src/main/java/com/xmxedu/oaken/request/adapters/Sdk10Adapter.java
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/request/adapters/Sdk10Adapter.java
@@ -4,7 +4,8 @@ import com.google.gson.JsonObject;
 import com.xmxedu.oaken.request.Adapter;
 
 /**
- * Created by xmzheng on 15/12/16.
+ * sdk 1.0 协议中将其数据转化成内部统一的对象
+ * @version 1.0
  */
 public class Sdk10Adapter implements Adapter {
     public JsonObject convert(JsonObject jsonObject) {
