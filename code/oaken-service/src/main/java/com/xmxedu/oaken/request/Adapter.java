b2f08a635ec08c6ba9a5096dc875476088b09eb4
Maclaon Zen <maclaonzen@gmail.com>
2015-12-16 22:40:07 +0800

    add adapter of it

--- code/oaken-service/src/main/java/com/xmxedu/oaken/request/Adapter.java
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/request/Adapter.java
@@ -1,8 +1,11 @@
 package com.xmxedu.oaken.request;
 
+import com.google.gson.JsonObject;
+
 /**
  * 协议适配，将不同的SDK，server2server的协议进行统一转换，
  * @version 1.0.0
  */
-public class Adapter {
+public interface Adapter {
+    public JsonObject convert(JsonObject jsonObject);
 }
---
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/request/adapters/Sdk10Adapter.java
@@ -0,0 +1,13 @@
+package com.xmxedu.oaken.request.adapters;
+
+import com.google.gson.JsonObject;
+import com.xmxedu.oaken.request.Adapter;
+
+/**
+ * Created by xmzheng on 15/12/16.
+ */
+public class Sdk10Adapter implements Adapter {
+    public JsonObject convert(JsonObject jsonObject) {
+        return null;
+    }
+}
